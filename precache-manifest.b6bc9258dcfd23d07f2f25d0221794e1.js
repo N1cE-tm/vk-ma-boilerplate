037d55d043d2fb747f4fba42340d181b2483682d
N1cE <banicov2011@yandex.ru>
2020-01-15 19:12:19 +0500

    Updates

---
+++ precache-manifest.b6bc9258dcfd23d07f2f25d0221794e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ae1a3fd422f24a5f47aa7a88996a0c",
+    "url": "/vk-ma-boilerplate/index.html"
+  },
+  {
+    "revision": "dc4bedb0ddfa339f43a0",
+    "url": "/vk-ma-boilerplate/static/css/2.1b740784.chunk.css"
+  },
+  {
+    "revision": "85f56fb951e811f35661",
+    "url": "/vk-ma-boilerplate/static/css/main.527bba1e.chunk.css"
+  },
+  {
+    "revision": "dc4bedb0ddfa339f43a0",
+    "url": "/vk-ma-boilerplate/static/js/2.39e19f9d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/vk-ma-boilerplate/static/js/2.39e19f9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "85f56fb951e811f35661",
+    "url": "/vk-ma-boilerplate/static/js/main.45a32ba6.chunk.js"
+  },
+  {
+    "revision": "0bece00d8b876e5e9242",
+    "url": "/vk-ma-boilerplate/static/js/runtime-main.ec4aea4b.js"
+  }
+]);
